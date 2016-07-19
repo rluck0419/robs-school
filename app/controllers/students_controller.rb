20d95c467f65d355da7d38c5c8ae164cbcd94e3f
Robert Luckfield <rluck0419@gmail.com>
2016-07-19 14:30:56 -0500

    crud for students & teachers too

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,58 @@
 class StudentsController < ApplicationController
+  def show
+    if Student.exists?(params[:id])
+      render template: 'students/show.html.erb', locals: { student: Student.find(params[:id]) }
+    else
+      render html: 'Student not found', status: 404
+    end
+  end
+
+  def index
+    render template: 'students/index.html.erb', locals: { students: Student.all }
+  end
+
+  def new
+    render locals: {
+      student: Student.find(params[:id])
+    }
+  end
+
+  def create
+    student = Student.new(student_params)
+    if student.save
+      redirect_to student
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render locals: {
+      student: Student.find(params[:id])
+    }
+  end
+
+  def update
+    student = Student.find(params[:id])
+    if student.update(student_params)
+      redirect_to student
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if Student.exists?(params[:id])
+      Student.destroy(params[:id])
+      flash[:notice] = "Student deleted"
+      redirect_to students
+    else
+      flash[:alert] = "There was an error - please try again"
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :teacher_id)
+  end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,58 @@
 class TeachersController < ApplicationController
+  def show
+    if Teacher.exists?(params[:id])
+      render template: 'teachers/show.html.erb', locals: { teacher: Teacher.find(params[:id]) }
+    else
+      render html: 'Teacher not found', status: 404
+    end
+  end
+
+  def index
+    render template: 'teachers/index.html.erb', locals: { teachers: Teacher.all }
+  end
+
+  def new
+    render locals: {
+      teacher: Teacher.find(params[:id])
+    }
+  end
+
+  def create
+    teacher = Teacher.new(teacher_params)
+    if teacher.save
+      redirect_to teacher
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render locals: {
+      teacher: Teacher.find(params[:id])
+    }
+  end
+
+  def update
+    teacher = Teacher.find(params[:id])
+    if teacher.update(teacher_params)
+      redirect_to teacher
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if Teacher.exists?(params[:id])
+      Teacher.destroy(params[:id])
+      flash[:notice] = "Teacher (& all students) deleted"
+      redirect_to teachers
+    else
+      flash[:alert] = "There was an error - please try again"
+    end
+  end
+
+  private
+  def teacher_params
+    params.require(:teacher).permit(:name, :school_id)
+  end
 end
